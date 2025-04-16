c9ed782dcd5810fbf9800fd3379a320138046dc1
k23suto <k23suto@xcat.co.jp>
2025-04-16 10:48:25 +0900

    create vcn

---
+++ 90.var_provider.tf
@@ -0,0 +1,9 @@
+# Region ID
+variable "region" {
+  default = "ap-tokyo-1"
+}
+
+# Compartment
+variable "terraform_compartment_ocid" {
+  default = "ocid1.compartment.oc1..aaaaaaaajs6qa7ucgjnxu5yiwbvgxnbqajxc4hfc7dtex7657ay3bbvgomqq"
+}
---
+++ 91.var_network.tf
@@ -0,0 +1,8 @@
+# VCN
+variable "vcn_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "vcn_display_name" {
+  default = "vcn-01"
+}
